d95c482ca19cef21c6bed98317513ca6261f47d1
BloodShot <bloodshot1387@hotmail.com>
2024-10-25 00:29:05 +0330

    update to version 2
    bunch of quality of life improvements you can see all the changes on the releases page .

--- main.py
+++ main.py
@@ -1,118 +1,156 @@
-import os
-import tkinter as ui
-from tkinter import *
-from tkinter import filedialog as fd
+import os, sys
+import customtkinter as ui
+from customtkinter import filedialog as fd
 from tkinter import messagebox as mb
-
-root = ui.Tk()
-root.title("Spooky Stories")
-root.geometry("900x600")
-root.config(background="orange")
-root.resizable(False, False)
+from collections import defaultdict
 dirr = os.path.dirname(__file__)
+
+ui.set_appearance_mode("dark")
+ui.set_default_color_theme(os.path.join(dirr, "themes", "main.json"))
+
+
+root = ui.CTk()
+root.title("Spooky Stories")
+root.resizable(False, False)
 root.iconbitmap(os.path.join(dirr, "spokystories.ico"))
-# made the root window and added the icon
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+position_x = (screen_width // 2) - (900 // 2)
+position_y = (screen_height // 2) - (600 // 2)
+root.geometry(f'900x600+{position_x}+{position_y}')
+
+
 try:
     from ctypes import windll
-
-    myappid = u'BloodShot.FunPython.SpookyStories.1'
+    myappid = u'BloodShot.SpookyStories.2'
     windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 except ImportError:
     pass
-# if the app is on windows, then we set a custom appid if not we pass
 
-# -------------------------------------
-title = ui.Label(text="Spooky Stories", background="white", foreground="black", relief="solid", borderwidth=5,
-                 font=("Monster Pumpkin", 40))
-title.grid(row=0, column=0, padx=0, pady=20)
-title.place(relx=0.5, rely=0.1, anchor=CENTER)
+title = ui.CTkLabel(master=root, text="Spooky Stories", font=("Monster Pumpkin", 50),height=70,width=100,anchor="s")
+title.place(relx=0.5, rely=0.1, anchor=ui.CENTER)
 
+ver = ui.CTkLabel(master=root, text="V2", font=("Monster Pumpkin", 20),anchor="s",fg_color="transparent")
+ver.place(relx=0.05, rely=0.95, anchor=ui.CENTER)
 
-# main label
-
-# --------------------------------------
 def load_story():
-    filet = (
-        ('story files', '*.txt'),
-    )
-    # noinspection PyGlobalUndefined
-    global story
-    story = fd.askopenfilename(title="select story file", filetypes=filet)
-    # noinspection PyGlobalUndefined
-    global button3
-    button3 = ui.Button(text="start", background="white", foreground="black", command=main_story,
-                        font=("Monster Pumpkin", 15))
-    button3.grid(row=3, column=0)
-    button3.place(relx=0.49, rely=0.6)
+    filet = (('story files', '*.txt'),)
 
+    def checkstory():
+        global story
+        story = fd.askopenfilename(title="select story file", filetypes=filet,initialdir=os.path.join(dirr, "stories"))
+        if story:
+            with open(story, 'r') as main:
+                s = main.readline().strip()
+                if (s != "#ssfile"):
+                    mb.showerror(title="Error", message="pls select a supported file")
+                    checkstory()
+                else:
+                    global button3
+                    button3 = ui.CTkButton(master=root, text="start", command=main_story, font=("Monster Pumpkin", 15))
+                    button3.place(relx=0.5, rely=0.6,anchor=ui.CENTER)
+        else:
+            mb.showerror(title="Error", message="Please select the story first")
+    
+    checkstory()
 
-button1 = ui.Button(text="select your story", background="white", foreground="black", command=load_story,
-                    font=("Monster Pumpkin", 20))
-button1.grid(row=1, column=0, padx=0, pady=50)
-button1.place(relx=0.39, rely=0.2)
-
-
-# load story button and function
+button1 = ui.CTkButton(master=root, text="select your story",width=230, command=load_story, font=("Monster Pumpkin", 30),height=50)
+button1.place(relx=0.5, rely=0.25, anchor=ui.CENTER)
 
 def main_story():
-    if len(story) > 0:
-        # checks if there is something in the story file directory if not we throw an error on line 128
-        textbox1 = ui.Entry(background="white", foreground="black",
-                            font=("Monster Pumpkin", 20))
-        textbox1.insert(0, 'name')
-        textbox1.grid(row=2, column=0, padx=0, pady=70)
-        textbox1.place(relx=0.3, rely=0.4)
-        # ---------------------------------------
-        next_pressed = StringVar()
-        button2 = ui.Button(text="Next", background="white", foreground="black",
-                            command=lambda: next_pressed.set("yes"),
-                            font=("Monster Pumpkin", 13))
-        button2.grid(row=2, column=1, padx=0, pady=70)
-        button2.place(relx=0.7, rely=0.4)
-        # a next button to go to the next question and works with a string var and wait_variable function
-        # ----------------------------------------
+    if story:
+        textbox1 = ui.CTkEntry(master=root, font=("Monster Pumpkin", 20))
+        textbox1.insert(0, "name")
+        textbox1.place(relx=0.3, rely=0.4, anchor=ui.CENTER)
+
+        next_pressed = ui.StringVar()
+        button2 = ui.CTkButton(master=root, text="Next", command=lambda: next_pressed.set("yes"), font=("Monster Pumpkin", 13))
+        button2.place(relx=0.7, rely=0.4, anchor=ui.CENTER)
+
+        def on_entry_click(event):
+            textbox1.delete(0, ui.END)
+        textbox1.bind("<FocusIn>", on_entry_click)
+
         name = textbox1.get()
         button2.wait_variable(next_pressed)
-        textbox1.delete(0, END)
+        textbox1.delete(0, ui.END)
+        button2.focus_set()
         textbox1.insert(0, "age")
         button2.wait_variable(next_pressed)
-        age = textbox1.get()
-        # -----------------------------------------
-        textbox1.delete(0, END)
-        textbox1.insert(0, "how do you describe your self?")
+        def checknumber():
+            if (textbox1.get().isnumeric()):
+                return textbox1.get()
+            else:
+                mb.showerror(title="Error", message="pls only enter digits")
+                textbox1.delete(0, ui.END)
+                button2.focus_set()
+                textbox1.insert(0, "age")
+                button2.wait_variable(next_pressed)
+                checknumber()
+        
+        age = checknumber()
+
+        textbox1.delete(0, ui.END)
+        textbox1.configure(width=160)
+        textbox1.insert(0, "describe your self")
+        button2.focus_set()
         button2.wait_variable(next_pressed)
         des = textbox1.get()
-        # -----------------------------------------
-        textbox1.delete(0, END)
+
+        textbox1.delete(0, ui.END)
+        textbox1.configure(width=140)
+        button2.focus_set()
         textbox1.insert(0, "woman/man")
         button2.wait_variable(next_pressed)
-        gender = textbox1.get()
-        # -----------------------------------------
-        textbox1.delete(0, END)
+        def checkgender():
+            if (textbox1.get() == "woman" or textbox1.get() == "man"):
+                return textbox1.get()
+            else:
+                mb.showerror(title="Error", message="pls only enter woman/man")
+                textbox1.delete(0, ui.END)
+                button2.focus_set()
+                textbox1.insert(0, "woman/man")
+                button2.wait_variable(next_pressed)
+                checkgender()
+    
+        gender = checkgender()
+
+        textbox1.delete(0, ui.END)
+        button2.focus_set()
         textbox1.insert(0, "obj")
         button2.wait_variable(next_pressed)
         obj = textbox1.get()
-        # -----------------------------------------
-        textbox1.delete(0, END)
+
+        textbox1.delete(0, ui.END)
+        button2.focus_set()
         textbox1.insert(0, "name the story")
         button2.wait_variable(next_pressed)
         story_name = textbox1.get() + "\n"
+
         with open(story, 'r') as main:
-            # we open the story file and use string concat for mad-libs (not the best implementation for sure)
-            s = main.readlines()[0]
-            stor = s
-            if gender == 'woman':
-                stor = stor.format(gender='girl', pronoun='her', name=name, age=age, des=des, pronoun2='she', obj=obj)
-            if gender == 'man':
-                stor = stor.format(gender='boy', pronoun='his', name=name, age=age, des=des, pronoun2='he', obj=obj)
+            s = main.readlines()
+            s = [i for i in s if "#ssfile" not in i]
+            stor = s[0]
+            story_values = defaultdict(str, {
+                'gender': 'girl' if gender == 'woman' else 'boy',
+                'pronoun': 'her' if gender == 'woman' else 'him',
+                'pronoun2': 'she' if gender == 'woman' else 'he',
+                'possession': 'her' if gender == 'woman' else 'his',
+                'name': name,
+                'age': age,
+                'des': des,
+                'obj': obj
+            })
+            stor = stor.format_map(story_values)
+
         export = fd.asksaveasfile(mode='w', defaultextension=".txt")
-        # then we save a new file
         if export is None:
-            # if the person clicks cancel, we throw an error and restart
-            mb.showerror(title="error", message="didnt save")
+            mb.showerror(title="error", message="didn't save")
             textbox1.destroy()
             button2.destroy()
             return
+
         export.write(story_name)
         export.write(stor)
         mb.showinfo(title="complete", message="your story is now saved")
@@ -120,11 +158,7 @@ def main_story():
         textbox1.destroy()
         button2.destroy()
         button3.destroy()
-        # we restart here
     else:
-        mb.showerror(title="Error", message="pls select the story first")
-
-
-# the main app after start button is clicked
+        mb.showerror(title="Error", message="Please select the story first")
 
 root.mainloop()
